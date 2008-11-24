1618cfaf4b8c0215bb3ed9aa6a64ccfac7579603
Chris Wanstrath <chris@ozmm.org>
2008-11-24 01:26:01 -0800

    use ido for fuzzy find in project - so fast and so awesome

--- textmate.el
+++ textmate.el
@@ -1,4 +1,4 @@
-;; textmate.el --- Basic TextMate emulation for Emacs
+;; textmate.el --- TextMate minor mode for Emacs
 ;; Chris Wanstrath <chris@ozmm.org>
 ;; Licensed under the same terms as Emacs.
 
@@ -16,29 +16,38 @@
 ;; In your emacs config:
 ;;
 ;; (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-;; (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el/vendor/fuzzy-find-in-project")
-;; (load "textmate")
+;; (require 'textmate)
 
-;;
-;; Dependencies
-;;
+;;; Minor mode
 
-(add-to-list 'load-path (expand-file-name "vendor/fuzzy-find-in-project"))
-(require 'fuzzy-find-in-project)
+(defvar textmate-mode-map (make-sparse-keymap))
+(defvar *textmate-project-root* nil)
+(defvar *textmate-fip-exclude-pattern* "vendor\\|fixtures\\|tmp\\|log")
 
-;;
-;; Keybindings
-;;
+;;; Bindings
 
-(global-set-key [A-return] 'textmate-insert-blank-line-after-current)
-(global-set-key (kbd "A-M-]") 'align)
-(global-set-key (kbd "A-]") 'indent-region)
-(define-key osx-key-mode-map (kbd "A-t") 'fuzzy-find-in-project)
-(define-key osx-key-mode-map (kbd "A-T") 'textmate-ido-goto-symbol)
+(defun textmate-bind-keys ()
+  (if (boundp 'aquamacs-version) 
+      (textmate-bind-aquamacs-keys)
+    (textmate-bind-carbon-keys)))
 
-;;
-;; Defun(kt)s
-;;
+(defun textmate-bind-aquamacs-keys ()
+  (define-key textmate-mode-map [A-return] 'textmate-insert-blank-line-after-current)
+  (define-key textmate-mode-map (kbd "A-M-]") 'align)
+  (define-key textmate-mode-map (kbd "A-]") 'indent-region)
+  ;; Needed to override menu items
+  (define-key osx-key-mode-map (kbd "A-t") 'textmate-find-in-project)
+  (define-key osx-key-mode-map (kbd "A-T") 'textmate-find-symbol))
+
+(defun textmate-bind-carbon-keys ()
+  ;; Are these any good? Anyone have good Carbon defaults?
+  (define-key textmate-mode-map [M-return] 'textmate-insert-blank-line-after-current)
+;  (define-key textmate-mode-map [(meta ])] 'align)
+  (define-key textmate-mode-map [(control tab)] 'indent-region)
+  (define-key textmate-mode-map [(meta t)] 'textmate-find-in-project)
+  (define-key textmate-mode-map [(meta T)] 'textmate-find-symbol))
+
+;;; Commands
 
 (defun textmate-insert-blank-line-after-current ()
   (interactive)
@@ -46,7 +55,7 @@
   (newline-and-indent))
 
 ;; http://chopmo.blogspot.com/2008/09/quickly-jumping-to-symbols.html
-(defun textmate-ido-goto-symbol ()
+(defun textmate-find-symbol ()
   "Will update the imenu index and then use ido to select a symbol to navigate to"
   (interactive)
   (imenu--make-index-alist)
@@ -72,7 +81,45 @@
                                (add-to-list 'symbol-names name)
                                (add-to-list 'name-and-pos (cons name position))))))))
       (addsymbols imenu--index-alist))
-    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+    (let* ((selected-symbol (ido-completing-read "Symbol: " symbol-names))
            (position (cdr (assoc selected-symbol name-and-pos))))
       (goto-char position))))
 
+;;; Utilities
+
+(defun textmate-find-in-project (&optional starting)
+  (interactive)
+  (textmate-find-project-root)
+  (find-file (concat *textmate-project-root* "/"
+                     (ido-completing-read "Find file: " (textmate-project-files *textmate-project-root*)))))
+
+(defun textmate-project-files (&optional root)
+  (cond
+   ((null root) '())
+   ((listp root) 
+    (mapcar
+     (lambda (path)
+       (replace-regexp-in-string (expand-file-name (concat *textmate-project-root* "/")) "" path))
+     (remq nil (flatten (cons (textmate-project-files (car root)) (textmate-project-files (cdr root)))))))
+   ((string-match *textmate-fip-exclude-pattern* root) '())
+   ((file-directory-p root) (textmate-project-files (directory-files root t "^[^.]+" t)))
+   (t root)))
+
+(defun textmate-find-project-root ()
+  (when (or (null *textmate-project-root*) (not (string-match default-directory *textmate-project-root*)))
+    (setq *textmate-project-root* (textmate-project-root))))
+
+(defun textmate-project-root (&optional root)
+  (when (null root) (setq root default-directory))
+  (cond
+   ((member ".git" (directory-files root)) root)
+   ((eq root "/") nil)
+   (t (textmate-project-root (concat root "..")))))
+
+;;;###autoload
+(define-minor-mode textmate-mode "TextMate Emulation Minor Mode"
+  :lighter " mate" :global t :keymap textmate-mode-map
+  (textmate-bind-keys))
+
+(provide 'textmate)
+;;; textmate.el ends here
